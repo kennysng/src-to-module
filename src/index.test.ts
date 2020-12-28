2ae11a6cd6b8de3eeaebcea4dc252de5c7905f20
Kennys Ng <kennys.ng@swivelsoftware.com>
2020-12-28 14:46:08 +0800

    test process.env.NODE_ENV

--- src/index.test.ts
+++ src/index.test.ts
@@ -9,3 +9,8 @@ test('async', async () => {
   const message = await requireAsync<string>('../test/async')
   expect(message).toBe('Hello, World')
 })
+
+test('context', () => {
+  const message = requireSync<string>('../test/context')
+  expect(message).toBe('test')
+})
